0fa622944f5b242d24b2b8be4c1826377f292278
lorenaparreno <137244224+lorenaparreno@users.noreply.github.com>
2024-05-19 22:15:36 -0400

    Add id to form.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary() {
     return (
         <div className="Dictionary">
             <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange} />
+                <input type="search" onChange={handleKeywordChange} id="search-engine" />
             </form>
         </div>
     );
